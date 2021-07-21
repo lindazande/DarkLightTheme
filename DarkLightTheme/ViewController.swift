05b54ae6ea7906bbbc0b48429a64c39f20c5a8fe
Linda Zande <linda.zande@accenture.com>
2021-07-21 18:44:38 +0300

    Initial Commit

---
+++ DarkLightTheme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DarkLightTheme
+//
+//  Created by linda.zande on 21/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
